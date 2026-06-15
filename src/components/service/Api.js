5108fae72514bc342d7971dc6f3c383ef4585238
Vitor Ferreira da Silva <vitor1996130@gmail.com>
2026-06-15 13:26:19 -0300

    atualizar projeto

--- src/components/service/Api.js
+++ src/components/service/Api.js
@@ -1,3 +1,6 @@
-const API_URL = "http://localhost:5000";
+/*const API_URL = "http://localhost:5000";
+
+export default API_URL;*/
+const API_URL = "https://costs-api-2.onrender.com";
 
 export default API_URL;
